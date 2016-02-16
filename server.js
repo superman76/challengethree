094cb6d11e8d2fbe12dba9baf4ad799c5710280d
fresh5447 <doug@kosmojo.com>
2016-02-16 14:44:32 -0700

    working get bear by id route

--- server.js
+++ server.js
@@ -54,6 +54,18 @@ router.route('/bears')
     })
   });
 
+router.route('/bears/:bear_id')
+  .get(function(req, res){
+    Bear.findById(req.params.bear_id, function(err, bear){
+      if(err){
+        console.log(err);
+      } else {
+        res.json(bear);
+      }
+    })
+  })
+  
+
 
 app.use('/api', router);
 
